0e5e1b1ff65250157dbb0e633eb98df40161e066
pavlo <nadazasada@gmail.com>
2026-06-23 19:57:16 +0300

    my first promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+const div = document.createElement('div');
+
+// div.className = 'message';
+
+const promise1 = new Promise((resolve) => {
+  logo.addEventListener('click', () => {
+    // const message = document.createElement('div');
+    div.className = 'message';
+    div.textContent = 'Promise was resolved!';
+    document.body.append(div);
+  });
+});
+
+const promise2 = new Promise((reject) => {
+
+});
+
